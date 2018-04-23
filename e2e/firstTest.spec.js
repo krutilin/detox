3c4a94ebad8861121f62910d994608b029fbcb93
Yuriy Krutilin <y.krutilin@gmail.com>
2018-04-23 12:24:23 +0300

    Huge delay for tests

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,21 +1,25 @@
-const { reloadApp } = require('detox-expo-helpers');
+const { reloadApp } = require("detox-expo-helpers");
 
-describe('Example', () => {
+describe("Example", () => {
+  function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
   beforeEach(async () => {
     await reloadApp();
+    await timeout(60000);
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it("should have welcome screen", async () => {
+    await expect(element(by.id("welcome"))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.label('Hello!!!'))).toBeVisible();
+  it("should show hello screen after tap", async () => {
+    await element(by.id("hello_button")).tap();
+    await expect(element(by.label("Hello!!!"))).toBeVisible();
   });
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.label('World!!!'))).toBeVisible();
+  it("should show world screen after tap", async () => {
+    await element(by.id("world_button")).tap();
+    await expect(element(by.label("World!!!"))).toBeVisible();
   });
 });
