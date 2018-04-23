7215cdfef806d441cfb4ff22f12d6b46b77a1a83
Yuriy Krutilin <y.krutilin@gmail.com>
2018-04-23 16:04:40 +0300

    .

--- e2e/init.js
+++ e2e/init.js
@@ -1,8 +1,15 @@
-require('babel-polyfill');
-const detox = require('detox');
-const config = require('../package.json').detox;
+require("babel-polyfill");
+const detox = require("detox");
+const config = require("../package.json").detox;
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 before(async () => {
+  console.log("before 1m wait");
+  await timeout(60000);
+  console.log("before detox.init");
   await detox.init(config);
 });
 
